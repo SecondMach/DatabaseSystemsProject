50c7e907ba6f50028830149903927921872214cb
Debra Ritter <zeeritter@gmail.com>
2025-11-18 14:00:41 -0500

    Changed code organization

--- format.py
+++ format.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-# Not in 1NF
-results_df = pd.read_csv("clean-data/results.csv")
-
 # Not in 3NF
 bios_df = pd.read_csv("clean-data/bios_locs.csv")
+
+# Not in 1NF
+results_df = pd.read_csv("clean-data/results.csv")
 print(bios_df.head())
