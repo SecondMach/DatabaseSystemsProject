3f94dce5c0ab8dea5252837c4c8e8771120a5f2f
Debra Ritter <zeeritter@gmail.com>
2025-11-23 19:55:59 -0500

    Populated format.py and created new CSV files for database normalization.

--- README.md
+++ README.md
@@ -3,3 +3,20 @@
 Taken from https://github.com/KeithGalli/Olympics-Dataset/tree/master/clean-data
 
 Data has not been uploaded to GitHub in this project
+
+## New Schema
+
+OlympicGame(__year__, season, hostCity)
+YearHosted(__year__, hostCity) **[NEW]**
+City(__hostCity__, hostCountry) **[NEW]**
+
+Sport(__year__, event, gender)
+DisciplineOf(__event__, discipline) **[NEW]**
+
+Athlete(__athlete_id__, name, DOB, countryBorn, nationPlayedFor)
+Country(__ISOCode__, fullName)
+
+EventPlayedIn(__year__, __hostCity__, __event__)
+AthleteParticipated(__year__, __event__, __athleteID__)
+Result(__athleteID__, __year__, __event__, placement)
+CitizenOf(__athleteID__, __ISOCode__)
--- format.py
+++ format.py
@@ -1,11 +1,11 @@
-"""
-A program to:
+"""A program to:
 - Load our CSVs containing our data into pandas DataFrames
 - Convert them to 3NF, and save the results as parquet files
 """
 
 __author__ = "Debra Ritter"
 
+import re
 import pandas as pd
 
 # Not in 3NF
@@ -13,8 +13,114 @@ bios_df: pd.DataFrame = pd.read_csv("clean-data/bios_locs.csv")
 
 # Not in 1NF
 results_df: pd.DataFrame = pd.read_csv("clean-data/results.csv")
-print(bios_df.head())
+athlete_events_df: pd.DataFrame = pd.read_csv("athlete_events.csv")
+noc_regions_df: pd.DataFrame = pd.read_csv("clean-data/noc_regions.csv")
 
 
 # Note that our relational database needs to be reformatted to have everything
 # fit in at least 3NF.
+OlympicGames = athlete_events_df[['Year', 'Season', 'City']].drop_duplicates()
+OlympicGames.to_csv('OlympicGames.csv', index=False)
+
+YearHosted = athlete_events_df[['Year', 'City']].drop_duplicates()
+YearHosted.to_csv('YearHosted.csv', index=False)
+
+Sport = athlete_events_df[['Year', 'Event']].drop_duplicates()
+
+def get_gender(event: str) -> str:
+    """Gets the gender from an event
+    Args:
+        event::str
+            An unformatted string which is the name of the event
+            
+    Returns:
+        gender::str
+            A single character string representing the gender of the event
+    """
+    if "Men" in event:
+        return 'M'
+    if "Women" in event:
+        return 'F'
+    return 'X'
+
+def format_event(event: str) -> str:
+    """Removes non gender andl non-alphabetical characters from event string
+    including spaces, apostrophes, etc
+
+    Args:
+        event::str
+            An unformatted string which is the name of the event
+
+    Returns:
+        formatted_event::str
+            A formatted event string
+    """
+    if 'Men\'s' in event:
+        event = event.replace('Men\'s', '')
+    elif 'Women\'s' in event:
+        event = event.replace('Women\'s', '')
+    elif 'Men' in event:
+        event = event.replace('Men', '')
+    elif 'Women' in event:
+        event = event.replace('Women', '')
+
+    formatted_event = re.sub(r'[^a-zA-Z0-9]', '', event)
+
+    return formatted_event
+
+Sport['Gender'] = Sport['Event'].apply(get_gender)
+Sport['Event'] = Sport['Event'].apply(format_event)
+Sport.to_csv('Sport.csv', index=False)
+
+DisciplineOf = results_df[['event', 'discipline']].drop_duplicates()
+
+DisciplineOf.rename(columns={
+    'discipline': 'Discipline',
+    'event': 'Event'
+}, inplace=True)
+
+DisciplineOf['Event'] = DisciplineOf['Event'].apply(format_event)
+DisciplineOf.to_csv('DisciplineOf.csv', index=False)
+
+Athlete = bios_df[['athlete_id', 'name', 'born_date', 'born_country', 'NOC']]
+
+renamed_athlete_cols = {col: col.capitalize() for col in Athlete.columns}
+Athlete.rename(columns=renamed_athlete_cols, inplace=True)
+Athlete.to_csv('Athlete.csv', index=False)
+
+Country = noc_regions_df[['NOC', 'region']].drop_duplicates()
+
+Country.rename(columns={
+    'region': 'FullName'
+}, inplace=True)
+
+Country.to_csv('Country.csv', index=False)
+
+EventPlayedIn = athlete_events_df[['Year', 'City', 'Event']].drop_duplicates()
+EventPlayedIn['Event'] = EventPlayedIn['Event'].apply(format_event)
+EventPlayedIn.to_csv('EventPlayedIn.csv', index=False)
+
+AthleteParticipated = results_df[['athlete_id', 'event', 'year']].drop_duplicates()
+AthleteParticipated.rename(columns={
+    'athlete_id': 'Athlete_ID',
+    'event': 'Event',
+    'year': 'Year'
+}, inplace=True)
+AthleteParticipated['Event'] = AthleteParticipated['Event'].apply(format_event)
+AthleteParticipated.to_csv('AthleteParticipated.csv', index=False)
+
+Result = results_df[['athlete_id', 'event', 'year', 'medal']].drop_duplicates()
+Result.rename(columns={
+    'athlete_id': 'Athlete_ID',
+    'event': 'Event',
+    'year': 'Year',
+    'medal': 'Medal'
+}, inplace=True)
+Result['Event'] = Result['Event'].apply(format_event)
+Result.to_csv('Result.csv', index=False)
+
+CitizenOf = bios_df[['athlete_id', 'NOC']].drop_duplicates()
+CitizenOf.rename(columns={
+    'athlete_id': 'Athlete_ID'
+}, inplace=True)
+CitizenOf.to_csv('CitizenOf.csv', index=False)
